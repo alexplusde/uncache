1d3e7f8cbd7cc1fc18db9a4818178106ed60f5d4
Alexander Walther <aw@alexplus.de>
2022-05-14 14:10:57 +0200

    fix "get config" syntax

--- boot.php
+++ boot.php
@@ -2,7 +2,7 @@
 
 # Seriously, why do you even think this could be a good idea and a solution to your problem?
 
-if (time() > rex_config::get('uncache, next')) {
+if (time() > rex_config::get('uncache', 'next')) {
     rex_delete_cache();
     rex_config::set('uncache', 'next', time() + rex_config::get('uncache', 'seconds'));
 }
