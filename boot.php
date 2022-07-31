d8ee5568c304b35f621481d15d3db4232917b683
Alexander Walther <aw@alexplus.de>
2022-07-31 14:35:05 +0200

    uncache first - make uncache great again

--- boot.php
+++ boot.php
@@ -1,8 +1,9 @@
 <?php
 
 # Seriously, why do you even think this could be a good idea and a solution to your problem?
-
+/*
 if (time() > rex_config::get('uncache', 'next')) {
     rex_delete_cache();
     rex_config::set('uncache', 'next', time() + rex_config::get('uncache', 'seconds'));
 }
+*/
