bb004803def0356ac5373b753239b53753553a38
Alexander Walther <aw@alexplus.de>
2022-07-31 15:25:49 +0200

    boot.php commenting

--- boot.php
+++ boot.php
@@ -1,7 +1,8 @@
 <?php
 
 # Seriously, why do you even think this could be a good idea and a solution to your problem?
-/*
+
+/* This was an April Fool's Joke and will stay here to remind you that not every problem comes from the REDAXO Cache even if you think so.
 if (time() > rex_config::get('uncache', 'next')) {
     rex_delete_cache();
     rex_config::set('uncache', 'next', time() + rex_config::get('uncache', 'seconds'));
